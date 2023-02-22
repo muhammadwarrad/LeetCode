0dfd1e502763511fc473b1330df3f24ce854448c
muhammadwarrad <moe_warrad@yahoo.com>
2023-02-21 18:37:50 -0600

    complete

--- easy/twoSum.js
+++ easy/twoSum.js
@@ -1,11 +1,20 @@
-// Given an array of integers nums and an integer target,return indices of the two numbers such that they add up to target.You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//  Given an array of integers nums and an integer target,return indices of the two numbers such that they add up to target.You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-// EXAMPLE1
-// Input: nums = [2,7,11,15], target = 9
-// Output: [0,1]
-// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-//EXAMPLE2
+//  EXAMPLE1
+//  Input: nums = [2,7,11,15], target = 9
+//  Output: [0,1]
+//  Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// EXAMPLE2
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
 
-
+function twoSum(nums, target) {
+    const complementMap = {};
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (complement in complementMap) {
+            return [complementMap[complement], i];
+        }
+        complementMap[nums[i]] = i;
+    }
+}
